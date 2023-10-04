a3b57f9b2405b719c5e3be8e9ca19fc2309cd7a3
Your Name <you@example.com>
2023-10-04 15:24:49 +0000

    fixup

--- currency_rate_update_riksbank/models/res_currency_rate_provider_Riksbank.py
+++ currency_rate_update_riksbank/models/res_currency_rate_provider_Riksbank.py
@@ -44,46 +44,42 @@ class ResCurrencyRateProviderRiksbank(models.Model):
         invert_calculation = False
         if base_currency != "SEK":
             invert_calculation = True
-            base_currency = 'SEK'
-            if base_currency not in currencies:
+            while 'SEK' in currencies:
+                currencies.remove('SEK')
+            while base_currency in currencies:
+                currencies.remove(base_currency)
+            if not currencies:
                 currencies.append(base_currency)
+            else:
+                raise UserError('Cannot retrieve a currency pair that is not compared with SEK ')
         content = defaultdict(dict)
 
         # NOTE: Step in 10 days is selected to reduce memory impact
-        step = timedelta(days=10)
         for currency in currencies:
-            since = date_from
-            until = since + step
-            while since <= date_to:
-                url = (
-                    "https://api-test.riksbank.se/swea/v1/Observations/%(source)s%(target)sPMI/%(from)s/%(to)s"
-                ) % {
-                    "source": base_currency,
-                    "target": currency,
-                    "from": str(since),
-                    "to": str(min(until, date_to)),
-                }
-                _logger.info("retrieving rates at" + url)
-                data = json.loads(self._riksbank_provider_retrieve(url))
-                if "error" in data and data["error"]:
-                    raise UserError(
-                        data["error_description"]
+            if currency == 'SEK':
+                continue
+
+            url = (
+                "https://api-test.riksbank.se/swea/v1/Observations/SEK%(target)sPMI/%(from)s/%(to)s"
+            ) % {
+                "target": currency,
+                "from": str(date_from),
+                "to": str(date_to),
+            }
+            _logger.info("DEBUG retrieving rates at: " + url)
+            data = json.loads(self._riksbank_provider_retrieve(url))
+            if "error" in data and data["error"]:
+                raise UserError(
+                    data["error_description"]
                         if "error_description" in data
                         else "Unknown error"
                     )
-                for entry in data:
-                     tmp = {}
-                     tmp[currency] = entry["value"]
-                     content[entry["date"]] = tmp
-                since += step
-                until += step
+            for entry in data:
+                 content[entry["date"]][currency] = 1/float(entry["value"])
 
         if invert_calculation:
             for k in content.keys():
-                base_rate = float(content[k][base_currency])
-                for rate in content[k].keys():
-                    content[k][rate] = str(float(content[k][rate]) / base_rate)
-                content[k]["SEK"] = str(1.0 / base_rate)
+                content[k] = {'SEK': 1/content[k][base_currency]}
         return content
 
     def _riksbank_provider_retrieve(self, url):
